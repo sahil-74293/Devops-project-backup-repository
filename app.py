01cace8147a1a9ebd20ed616a22d423151b67fc0
sahil-74293 <122378244+sahil-74293@users.noreply.github.com>
2024-02-25 12:40:30 +0000

    created a html with the job website title

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-  return "Hello, World!"
+@app.route("/")
+def hello_User():
+  return render_template('home.html')
 if __name__ == '__main__':
- app.run(host='0.0.0.0', debug = True)
+ app.run(host='0.0.0.0',debug=True,port=8080 )
