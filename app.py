0c1948e5d77235106acba84dca2752ee29e826da
sahil-74293 <122378244+sahil-74293@users.noreply.github.com>
2024-02-25 12:45:19 +0000

    latest update

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello_User():
   return render_template('home.html')
+
+
 if __name__ == '__main__':
- app.run(host='0.0.0.0',debug=True,port=8080 )
+  app.run(host='0.0.0.0', debug=True, port=8080)
