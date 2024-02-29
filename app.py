a4a293ae7a357372152c2f1439aefe8e6a3b1dcc
sahil-74293 <122378244+sahil-74293@users.noreply.github.com>
2024-02-29 11:03:24 +0000

    added dynamic data , API route and made layout changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,jsonify
 
 app = Flask(__name__)
 
@@ -41,6 +41,8 @@ def hello_User():
                          jobs = JOBS,
                          company_name='Talent Quest Hub')
 
-
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True, port=8080)
