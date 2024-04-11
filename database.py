8e842a52459b00ed71bfb97b0f1c1a3f5ad38743
sahil-74293 <122378244+sahil-74293@users.noreply.github.com>
2024-04-11 11:22:43 +0000

    made styling and layout changes , added dynamic pages for each job.

--- database.py
+++ database.py
@@ -10,4 +10,13 @@ def load_jobs_from_db():
   for row in result.all():
     jobs.append(row._asdict())
   return jobs
-  
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),{'val':id})
+    row = result.fetchone()
+    if row:
+      return row._asdict()
+    else:
+      return None
